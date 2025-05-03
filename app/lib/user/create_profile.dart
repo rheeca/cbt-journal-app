ff89912aa397f7219e3c079efbcd385fb5256c56
Rheeca Guion <144939073+rheeca@users.noreply.github.com>
2025-05-02 23:16:30 -0400

    update ui of create profile

--- app/lib/user/create_profile.dart
+++ app/lib/user/create_profile.dart
@@ -1,5 +1,6 @@
 import 'package:cbt_journal/generated/user.pb.dart' as pb_user;
 import 'package:cbt_journal/home/home_controller.dart';
+import 'package:cbt_journal/theme.dart';
 import 'package:cbt_journal/util/util.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -29,48 +30,56 @@ class _CreateProfileScreenState extends State<CreateProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 50.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('What is your name?'),
-              const SizedBox(height: 12.0),
-              TextField(
-                controller: controller,
-                autofocus: true,
-                maxLines: 1,
-                decoration: const InputDecoration(
-                  hintText: 'Name',
-                ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Card(
+            color: AppColor.lightYellow.color,
+            child: Container(
+              padding: const EdgeInsets.all(40.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text('What is your name?'),
+                  const SizedBox(height: 12.0),
+                  TextField(
+                    controller: controller,
+                    autofocus: true,
+                    maxLines: 1,
+                    decoration: const InputDecoration(
+                      hintText: 'Name',
+                    ),
+                  ),
+                  const SizedBox(height: 12.0),
+                  FilledButton(
+                    onPressed: () {
+                      final firebaseUser = FirebaseAuth.instance.currentUser;
+                      if (firebaseUser != null) {
+                        final now = DateTime.now().toUtc();
+                        di<HomeController>().createProfile(
+                          pb_user.User(
+                            id: firebaseUser.uid,
+                            email: firebaseUser.email ?? '',
+                            displayName: controller.text,
+                            createdAt: now.toProtoTimestamp(),
+                            updatedAt: now.toProtoTimestamp(),
+                            isDeleted: false,
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text('Save'),
+                  ),
+                ],
               ),
-              const SizedBox(height: 12.0),
-              FilledButton(
-                onPressed: () {
-                  final firebaseUser = FirebaseAuth.instance.currentUser;
-                  if (firebaseUser != null) {
-                    final now = DateTime.now().toUtc();
-                    di<HomeController>().createProfile(
-                      pb_user.User(
-                        id: firebaseUser.uid,
-                        email: firebaseUser.email ?? '',
-                        displayName: controller.text,
-                        createdAt: now.toProtoTimestamp(),
-                        updatedAt: now.toProtoTimestamp(),
-                        isDeleted: false,
-                      ),
-                    );
-                  }
-                },
-                child: const Text('Save'),
-              ),
-              const SizedBox(height: 150.0),
-            ],
+            ),
           ),
         ),
-      ),
+        const SizedBox(height: 70),
+      ],
     );
   }
 }
